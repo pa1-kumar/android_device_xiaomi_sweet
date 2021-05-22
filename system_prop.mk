cd2e0f5e75ea236b51c1ba51c824e694de8164bb
pa1 <bhukyapavan105@gmail.com>
2021-05-22 10:18:52 +0000

    add pa doze props

--- system_prop.mk
+++ system_prop.mk
@@ -14,7 +14,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
-# Enable blurs
+# ParanoidDoze
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1  
+    ro.sensor.pickup=xiaomi.sensor.pickup \
+    ro.sensor.proximity=true
